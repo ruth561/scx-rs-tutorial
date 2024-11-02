f034fa17d8cd6a60c9fb19dde7792a9052ffe6cb
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2024-11-02 10:29:15 +0900

    Implemented init_task and exit_task

--- src/bpf/main.bpf.c
+++ src/bpf/main.bpf.c
@@ -18,7 +18,24 @@ void BPF_STRUCT_OPS(tutorial_exit, struct scx_exit_info *ei)
 	UEI_RECORD(uei, ei);
 }
 
+s32 BPF_STRUCT_OPS(tutorial_init_task, struct task_struct *p,
+		   struct scx_init_task_args *args)
+{
+	bpf_printk("[ init_task ] pid=%d, fork=%d, comm=%s",
+		p->pid, args->fork, p->comm);
+	return 0;
+}
+
+void BPF_STRUCT_OPS(tutorial_exit_task, struct task_struct *p,
+		    struct scx_exit_task_args *args)
+{
+	bpf_printk("[ exit_task ] pid=%d, canceled=%d",
+		p->pid, args->cancelled);
+}
+
 SCX_OPS_DEFINE(tutorial_ops,
 	       .init			= (void *)tutorial_init,
 	       .exit			= (void *)tutorial_exit,
+		   .init_task		= (void *)tutorial_init_task,
+		   .exit_task		= (void *)tutorial_exit_task,
 	       .name			= "tutorial");
