20d25be06fabb175fb46df2366b7367b914903f2
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2024-11-06 20:21:48 +0900

    Refactoring

--- src/bpf_skel.rs
+++ src/bpf_skel.rs
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
 
-// build.rsによって生成されたスケルトンファイルの中身をここにincludeする。
+/*
+ * The build sctipt build.rs compiles the BPF program src/bpf/main.bpf.c
+ * and generates a skeleton file at OUT_DIR/bpf_skel.rs.
+ * The following statement includes the generated file here. 
+ */
 include!(concat!(env!("OUT_DIR"), "/bpf_skel.rs"));
